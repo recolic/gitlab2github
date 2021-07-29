1a1b0fc6a76ea5ce79365b4c13d50f4201baa66d
Recolic K <bensl@microsoft.com>
2021-07-29 17:52:19 +0800

    update

--- gitlab2github_daemon.fish
+++ gitlab2github_daemon.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/fish
 
 while true
+    # git.recolic.net and gitsync located on the same machine. If the machine is rebooted, this script will fail and retry again and again. 
+    # sleep 60 to avoid deadly crazy retry loop
+    sleep 60
+
     fish ./gitlab2github_sync.fish
     echo "Sleeping 3600s..." 1>&2
     sleep 3600
